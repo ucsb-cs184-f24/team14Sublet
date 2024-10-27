fe9008cf48241243e065c13848f593d960fbaefc
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-10-26 17:15:10 -0700

    delete stylesheet

--- Homelette/app/index.tsx
+++ Homelette/app/index.tsx
@@ -12,11 +12,3 @@ export default function Index() {
 
   return <WelcomePage />;
 }
-
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
