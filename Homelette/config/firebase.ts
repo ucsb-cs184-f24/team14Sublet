8f6a4e33a21f2612d205206f93cab7d0a1c7ca08
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-10-30 12:02:40 -0700

    Add first and last name textfields

--- Homelette/config/firebase.ts
+++ Homelette/config/firebase.ts
@@ -1,7 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { create } from "react-test-renderer";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,18 +20,36 @@ const firebaseConfig = {
   storageBucket: "team14-sublet-1034a.appspot.com",
   messagingSenderId: "227281162992",
   appId: "1:227281162992:web:ad7bc4f3a19de5f557a835",
-  measurementId: "G-NN10YB68DC"
+  measurementId: "G-NN10YB68DC",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
+export const firestore = getFirestore(app);
 
-export const signUp = async (email: string, password: string) => {
+export const signUp = async (
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+) => {
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    return userCredential.user;
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    const user = userCredential.user;
+
+    const userProfile = {
+      email: email,
+      first: firstName,
+      last: lastName,
+      join_date: new Date(),
+    };
+
+    await setDoc(doc(firestore, "users", user.uid), userProfile);
   } catch (error) {
     throw error;
   }
@@ -33,7 +57,11 @@ export const signUp = async (email: string, password: string) => {
 
 export const signIn = async (email: string, password: string) => {
   try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
     return userCredential.user;
   } catch (error) {
     throw error;
