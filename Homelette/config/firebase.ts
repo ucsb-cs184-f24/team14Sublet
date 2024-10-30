d0069c47284e8c5b56409c0dcef357499addb13e
aqiu04 <allenqiu04@gmail.com>
2024-10-29 19:23:52 -0700

    Interested listings backend

--- Homelette/config/firebase.ts
+++ Homelette/config/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 
 export const auth = getAuth(app);
 
@@ -39,3 +42,50 @@ export const signIn = async (email: string, password: string) => {
     throw error;
   }
 };
+
+export async function getLeases() {
+  try {
+    uid = auth.currentUser?.uid;
+    // console.log(uid);
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
+    // console.log(docRef);
+
+    let leases = []
+
+    if (docSnap.exists()) {
+      listings = docSnap.data()['interested_listing_ids'];
+      console.log(listings);
+      for (i in listings) {
+        let listingRef = doc(db, "listings", listings[i]);
+        let listingSnap = await (getDoc(listingRef));
+        let listing = listingSnap.data();
+
+        let propertyRef = doc(db, "properties", listing['property_id']);
+        console.log(listing['property_id'])
+        let propertySnap = await (getDoc(propertyRef));
+        let property = propertySnap.data()
+        
+
+        leases.push({
+          id: i,
+          property: property['address']['street_address'],
+          rent: listing['price'],
+          startDate: listing['start_date'],
+          endDate: listing['end_date'],
+          image: property['image_url'],
+          bedCount: property['bedrooms'],
+          bathCount: property['bathrooms'],
+          area: property['area']
+        });
+      }
+    }
+    else {
+      console.log("NOT FOUND");
+    }
+    console.log(leases);
+    return leases;
+  } catch (error) {
+    throw error;
+  }
+}
