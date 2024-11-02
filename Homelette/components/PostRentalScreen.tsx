4c579eb30a9a046878649cf5093691a08fe0be28
YoungHypo <haiboyang@smail.nju.edu.cn>
2024-11-02 16:08:37 -0700

    change date format

--- Homelette/components/PostRentalScreen.tsx
+++ Homelette/components/PostRentalScreen.tsx
@@ -161,7 +161,7 @@ export default function PostRentalScreen() {
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
 
-    return `${month}-${day}-${year}`;
+    return `${year}-${month}-${day}`;
   };
 
   const confirmIOSDate = (key) => {
